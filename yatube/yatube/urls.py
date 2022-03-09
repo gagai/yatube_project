30b815a5becaf41cc954cd739ead4f72c77088b9
Vlad Gagai <gagai.vlad@gmail.com>
2022-03-09 22:22:53 +0300

    adding the project to pythonanywhere.com

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -31,6 +31,8 @@ handler404 = 'core.views.page_not_found'
 handler500 = 'core.views.internal_server_error'
 
 if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
     urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT,
     )
