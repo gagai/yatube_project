d520b979a2bb8ae836e2051a2128d284270c9685
Влад Гагай <gagai@MacBook-Air-Vlad.local>
2023-01-20 02:28:31 +0300

    set debug to True

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hvqk_-zk4h+&(b1x=x!ub8p1xo8afv-+(ho7eairfb7$&ei4!d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
